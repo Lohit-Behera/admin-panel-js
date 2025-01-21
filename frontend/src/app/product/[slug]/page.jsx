5b9169ba0b84d37f42eccf8ae7b8d81db82c261b
lohit <lohitsekhar.2002@gmail.com>
2025-01-21 19:26:39 +0530

    update SearchProduct component to display a message when no products are found

--- frontend/src/app/product/[slug]/page.jsx
+++ frontend/src/app/product/[slug]/page.jsx
@@ -22,19 +22,28 @@ function SearchProduct({ params }) {
       ) : searchProductStatus === "failed" ? (
         <div>Error</div>
       ) : searchProductStatus === "succeeded" ? (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {searchProduct.map((product) => (
-            <div key={product._id} className="grid gap-2 border p-4 rounded-md">
-              <img
-                src={product.thumbnail}
-                alt={product.name}
-                className="w-full h-44 rounded-md object-cover"
-              />
-              <h3 className="text-sm md:text-base font-semibold">
-                {product.name}
-              </h3>
+        <div className="w-full md:w-[95%]">
+          {searchProduct.length === 0 ? (
+            <h5 className="font-semibold text-center">No product found</h5>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              {searchProduct.map((product) => (
+                <div
+                  key={product._id}
+                  className="grid gap-2 border p-4 rounded-md"
+                >
+                  <img
+                    src={product.thumbnail}
+                    alt={product.name}
+                    className="w-full h-44 rounded-md object-cover"
+                  />
+                  <h3 className="text-sm md:text-base font-semibold">
+                    {product.name}
+                  </h3>
+                </div>
+              ))}
             </div>
-          ))}
+          )}
         </div>
       ) : null}
     </>
